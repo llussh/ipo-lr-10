e272e37ea077529e16220a6acbfa149be7d91a48
Дарья <dteleshevskaya.internet.ru>
2025-12-19 13:43:15 +0300

    Task 3 complete

--- collision/collision.py
+++ collision/collision.py
@@ -2,4 +2,17 @@ def isCorrectRect(rect):
     (x1, y1), (x2, y2) = rect
     return x1 < x2 and y1 < y2
 
+class RectCorrectError(Exception):
+    pass
+
+def isCollisionRect(rect1, rect2):
+    if not isCorrectRect(rect1):
+        raise RectCorrectError("1й прямоугольник некорректный")
+    if not isCorrectRect(rect2):
+        raise RectCorrectError("2й прямоугольник некорректный")
+
+    (x1, y1), (x2, y2) = rect1
+    (x3, y3), (x4, y4) = rect2
+
+    return not (x2 < x3 or x4 < x1 or y2 < y3 or y4 < y1)
 
