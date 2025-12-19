0008359791d12e6d7729dfed82a603eb55cda181
Дарья <dteleshevskaya.internet.ru>
2025-12-19 13:45:02 +0300

    Task 4 complete

--- collision/collision.py
+++ collision/collision.py
@@ -16,3 +16,23 @@ def isCollisionRect(rect1, rect2):
 
     return not (x2 < x3 or x4 < x1 or y2 < y3 or y4 < y1)
 
+def intersectionAreaRect(rect1, rect2):
+    if not isCorrectRect(rect1):
+        raise ValueError("1й прямоугольник некорректный")
+    if not isCorrectRect(rect2):
+        raise ValueError("2й прямоугольник некорректный")
+
+    (x1, y1), (x2, y2) = rect1
+    (x3, y3), (x4, y4) = rect2
+
+    x_left = max(x1, x3)
+    x_right = min(x2, x4)
+    y_bottom = max(y1, y3)
+    y_top = min(y2, y4)
+
+    if x_right <= x_left or y_top <= y_bottom:
+        return 0.0
+
+    return (x_right - x_left) * (y_top - y_bottom)
+
+
